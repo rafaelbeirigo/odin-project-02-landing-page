dfb7d2659fcf3a7db97fd1d88a4aeb2817cf45e5
Rafael Beirigo <rafaelbeirigo@gmail.com>
2025-11-06 21:02:33 -0300

    feat: add header

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((org-mode . ((auto-fill-function . nil)
+              (my-org-babel-default-language . "html")
+              (my-org-babel-default-tangling-p . nil)
               (eval . (add-hook 'after-save-hook
-                                (lambda ()
-                                  (org-babel-tangle)
-                                  (noweb-load-auto-tangled))
+                                (lambda () (org-babel-tangle))
                                 nil t)))))
